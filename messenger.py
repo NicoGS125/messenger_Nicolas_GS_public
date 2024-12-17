077186b2ce8c5d790f7e483fe90ddc5ba088e63b
14alarro <14alarro>
2024-12-17 19:05:37 +0100

    fix: make Server and Client classes work

--- messenger.py
+++ messenger.py
@@ -49,39 +49,54 @@ class Server :
         self.channels = channels
         self.message = message
 
+    @classmethod
+    def load_from_json_file(cls, json_file_path: str = 'server_json.json') -> 'Server':
+        with open(json_file_path) as json_file:
+            server_as_class = cls([],[],[])
+            server = json.load(json_file)
+            server_as_class.users = [User.conversion_dico_User(elem) for elem in server["users"]]
+            server_as_class.channels = [Channels.conversion_dico_Channels(channels) for channels in server['channels']]
+            server_as_class.message = [Message.conversion_dico_Message(message) for message in server['message']]
+        return server_as_class
 
-def load_server() :
-    with open('server_json.json') as json_file:
-        server_as_class = Server([],[],[])
-        server = json.load(json_file)
-        server_as_class.users = [User.conversion_dico_User(elem) for elem in server["users"]]
-        server_as_class.channels = [Channels.conversion_dico_Channels(channels) for channels in server['channels']]
-        server_as_class.message = [Message.conversion_dico_Message(message) for message in server['message']]
-    return server_as_class
+    def save_to_json_file(self, json_file_path: str = 'server_json.json'):
+        server_as_dict = {
+            'users': [],
+            'channels': [],
+            'message': [],
+        }
+        for user in self.users:
+            server_as_dict['users'].append(User.conversion_User_dico(user))
+        for channel in self.channels:
+            server_as_dict['channels'].append(Channels.conversion_Channels_dico(channel))
+        for message in self.message:
+            server_as_dict['message'].append(Message.conversion_Message_dico(message))
+        with open(json_file_path, 'w') as json_file :
+            json.dump(server_as_dict, json_file)
 
-server = load_server()
-
-def id_to_name2(id):
-    L = []
-    for elem in server.users:
-        L.append(elem.name)
-    return L[0]
+    def id_to_name2(self, id):
+        L = []
+        for elem in self.users:
+            L.append(elem.name)
+        return L[0]
 
 class Client :
-    def __init__(self, server : 'dict'):
+    def __init__(self, server : Server):
         self.server = server
-    @classmethod
-    def nv_channel(name, member_ids):
-        new_channel_id = max(channel.id for channel in server.channels) + 1
+
+    def nv_channel(self, name, member_ids):
+        new_channel_id = max(channel.id for channel in self.server.channels) + 1
         new_channel = {"id": new_channel_id, "name": name, "member_id": member_ids}
-        server.channels.append(Channels.conversion_dico_Channels(new_channel))
+        self.server.channels.append(Channels.conversion_dico_Channels(new_channel))
         print(f"\nNouveau canal ajouté : {new_channel}")
         return new_channel
-    def new_user(server):
+
+    def new_user(self):
         nom = input("choisissez un prénom")
-        n_id = max(user.id for user in server['users'])+1
-        server.users.append(User.conversion_dico_User({'id': n_id,'name':nom}))
-    def accueil(server) :
+        n_id = max(user.id for user in self.server['users'])+1
+        self.server.users.append(User.conversion_dico_User({'id': n_id,'name':nom}))
+
+    def accueil(self) :
         print('=== Messenger ===')
         print('x. Leave')
         print('1.See users')
@@ -91,15 +106,15 @@ class Client :
         if choice == 'x':
             print('Bye!')
         elif choice == '1':
-            for elem in server.users :
+            for elem in self.server.users :
                 print(elem.id,",",elem.name)
         elif choice == '2':
-            for elem in server.channels:
+            for elem in self.server.channels:
                 print(elem.name)
         elif choice == '3':
             choice2 = input("de quel groupe voulez vous voir le message (entrer l'id)")
             L = []
-            M = [m for m in server.message if m.channel == int(choice2)]
+            M = [m for m in self.server.message if m.channel == int(choice2)]
             print(M)
             for elem in M:
                 L.append(elem.content)
@@ -107,33 +122,6 @@ class Client :
         else:
             print('Unknown option:', choice)
 
-    
-def sauvegarde_fichier():
-    with open('server_json.json', 'w') as file :
-        server_as_dict = {}
-        server_as_dict['users'] = []
-        for elem in server.users : 
-            server_as_dict['users'].append(User.conversion_User_dico(elem))
-        for elem in server.channels :
-            server_as_dict['channels'].append(Channels.conversion_Channels_dico(elem))
-        for elem in server.message :
-            server_as_dict['message'].append(Message.conversion_Message_dico(elem))
-        json.dump()
-
-
-
-    
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
+server = Server.load_from_json_file()
+client = Client(server)
+client.accueil()
